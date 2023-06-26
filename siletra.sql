6c9a15bd1fbf5d88a737fbb4699f91237939809d
Al - Amin <2111521017_al-amin@student.unand.ac.id>
2023-06-26 15:14:23 +0700

    database

--- siletra.sql
+++ siletra.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2023 at 10:02 AM
+-- Generation Time: Jun 26, 2023 at 10:13 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
